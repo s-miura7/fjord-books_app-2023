3fad32733128e5100bf4ce906c3788ea6a90cd13
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-24 10:15:50 +0900

    activeRecordモデルで翻訳

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # config/routes.rb
+# scope "(:locale)", locale: /en|ja/ do
+#   resources :books
+# end
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
