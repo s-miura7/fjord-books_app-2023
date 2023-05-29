1444c5e18fdaa049f1488844f77504aa8b870716
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-29 13:10:30 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-def default_url_options
-   { locale: set_locale }
-end
+  def default_url_options
+    { locale: set_locale }
+  end
 
-private def set_locale
-  params[:locale] || I18n.default_locale
-end
+  private
+
+  def set_locale
+    params[:locale] || I18n.default_locale
+  end
 end
