bcb455ad0ca751c8aa3ed2d0029f78e0e1ca4185
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-29 16:39:55 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validate :avatar_content_type, if: :was_attached?
+
+  def avatar_content_type
+    extension = ['image/jpg', 'image/png', 'image/gif']
+    errors.add(:avatar, "の拡張子が間違っています") unless avatar.in?(extension)
+  end
+
+  def was_attached?
+    self.avatar.attached?
+  end
 end
