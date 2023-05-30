a6c56a51a4d6a46e60574434bc9c242726cda6fe
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-30 11:06:59 +0900

    query stringに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|ja/ do
-    resources :books
-  end
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
