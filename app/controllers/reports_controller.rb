0c30010d74a1571f77d27de1322803e8f3c307b4
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-30 11:41:50 +0900

    jsonを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,10 +26,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.save
         format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,10 +37,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.update(report_params)
         format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +49,6 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
