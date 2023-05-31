76ac16cde79a1f2531f0ae29e28871617c101652
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-31 09:41:42 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def default_url_options
-    { locale: set_locale }
+  before_action :set_locale
+
+  def default_url_options(options={})
+    options.merge(locale: set_locale)
   end
 
   private
 
   def set_locale
-    params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
