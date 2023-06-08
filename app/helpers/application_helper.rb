ac986bbe94188ee5e003201ede00b558d713970f
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-08 10:35:52 +0900

    正規表現を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
   end
 
   def create_link(text)
-    url_texts = text.scan(%r{(?:http://localhost:3000/reports/)(?:\d+)}).flatten
+    url_texts = text.scan(%r{http://localhost:3000/reports/\d+})
     url_texts.each do |url_text|
       text.gsub!(url_text) { "<a href='#{::Regexp.last_match(0)}'>#{::Regexp.last_match(0)}</a>" }
     end
