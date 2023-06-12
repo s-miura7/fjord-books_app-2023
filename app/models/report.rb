d8b0f0de1ae22479ded4af2306856892f9307165
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-12 11:39:04 +0900

    reportクラスにバリデーションを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  with_options presence: true do
+    validates :title
+    validates :content
+  end
 end
