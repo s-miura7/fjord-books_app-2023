a091fe589554d8bdff94fe4aecea5d6332f2a3b7
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-13 15:59:34 +0900

    モデルのテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @report1 = reports(:one)
+    @report2 = reports(:two)
+    @report3 = reports(:three)
+  end
+
+  test "#editable?" do
+    assert @report1.editable?(@alice)
+    assert_not @report1.editable?(@bob)
+  end
+
+  test "#created_on" do
+    assert_equal '2023-06-13'.to_date, @report1.created_on
+    @report2.created_at = "test"
+    assert_raises('NoMethod'){@report2.created_on}
+  end
+
+  test "#save_mentions" do
+    assert_equal @report1, @report2.mentioning_reports.first
+    @report2.content = "http://localhost:3000/reports/113629430"
+    @report2.send(:save_mentions)
+    assert_equal @report3, @report2.mentioning_reports.second
+  end
 end
