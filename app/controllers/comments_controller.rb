f5eec9c3700965d15f5a9e9b3a463dca939a4d47
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-13 17:01:24 +0900

    不要な行を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,8 +29,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-      params.require(:comment).permit(:content, :commentable_id, :commentable_type)
+      params.require(:comment).permit(:content)
   end
 end
-
-
