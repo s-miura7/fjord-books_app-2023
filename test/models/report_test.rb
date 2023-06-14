5943c0641190f1ad5b6ffbf5ec5da21014f659ea
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-14 11:09:55 +0900

    テストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,16 +17,23 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#created_on' do
-    assert_equal '2023-06-13'.to_date, @report1.created_on
+    assert_equal Time.zone.today.to_date, @report1.created_on
     @report2.created_at = 'test'
     assert_raises('NoMethod') { @report2.created_on }
   end
 
   test '#save_mentions' do
     @report2.send(:save_mentions)
+    assert_equal @report2.mentioning_reports.first.id, @report2.active_mentions.first.mentioned_by_id
     assert_equal @report1, @report2.mentioning_reports.first
     @report2.content = 'http://localhost:3000/reports/113629430'
     @report2.send(:save_mentions)
-    assert_equal @report3, @report2.mentioning_reports.second
+    assert_equal @report2.mentioning_reports.second.id, @report2.active_mentions.first.mentioned_by_id
+    @report2.content = 'http://localhost:3000/reports/113629430 http://localhost:3000/reports/113629430'
+    @report2.send(:save_mentions)
+    assert_equal 1, @report2.active_mentions.length
+    @report2.content = 'http://localhost:3000/reports/a'
+    @report2.send(:save_mentions)
+    assert_empty @report2.active_mentions
   end
 end
