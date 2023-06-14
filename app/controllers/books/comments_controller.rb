9aa2420cfc1857d839e44813e5c95202008ec8c7
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-14 14:14:46 +0900

    destroyメソッドの作成

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,15 +1,27 @@
 class Books::CommentsController < ApplicationController
+  before_action :set_book, only: %i[create destroy]
   def create
-    book = Book.find(params[:book_id])
-    if book.comments.build(content: comment_params[:content], user: current_user).save
-      redirect_to book_url(book), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    if @book.comments.build(content: comment_params[:content], user: current_user).save
+      redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
       render book_url(reoprt), status: :bad_request
+    end
+  end
+
+  def destroy
+    if current_user.comments.find(params[:id]).destroy
+      redirect_to book_url(@book), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      render book_url(params[:id]), status: :bad_request
     end
   end
   private
 
   def comment_params
       params.require(:comment).permit(:content)
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 end
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,4 +1,5 @@
 class Reports::CommentsController < ApplicationController
+  before_action :set_report, only: %i[create destroy]
   def create
     report = Report.find(params[:report_id])
     if report.comments.build(content: comment_params[:content], user: current_user).save
@@ -7,9 +8,21 @@ class Reports::CommentsController < ApplicationController
       render book_url(reoprt), status: :bad_request
     end
   end
+
+  def destroy
+    if current_user.comments.find(params[:id]).destroy
+      redirect_to report_url(params[:id]), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      render report_url(params[:id]), status: :bad_request
+    end
+  end
   private
 
   def comment_params
       params.require(:comment).permit(:content)
+  end
+
+  def set_report
+    @report = Report.find(params[:report_id])
   end
 end
