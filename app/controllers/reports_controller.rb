c8d9a0a17464c67157468f728528c6ef605abf05
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-14 16:06:32 +0900

    編集でURLが消えたときに必要なメソッドをmentionからdelete_mentionに変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -36,7 +36,7 @@ class ReportsController < ApplicationController
     new_keys = after_keys.difference(before_keys)
     if @report.update(report_params)
       new_keys.each { |key| @report.mention(key) } unless new_keys.empty?
-      deleted_keys.each { |key| @report.mention(key) } unless deleted_keys.empty?
+      deleted_keys.each { |key| @report.delete_mention(key) } unless deleted_keys.empty?
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
