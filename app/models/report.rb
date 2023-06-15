1ee7a8c4c775a95770ebbe59fa13f0f1797ef5e7
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-15 20:02:08 +0900

    mentioning, mentionedを複数形に修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,11 +4,11 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning, class_name: 'ReportMention', foreign_key: 'mentioned_id', dependent: :destroy, inverse_of: :mentioned
-  has_many :mentioned, class_name: 'ReportMention', foreign_key: 'mentioning_id', dependent: :destroy, inverse_of: :mentioning
+  has_many :mentionings, class_name: 'ReportMention', foreign_key: 'mentioned_id', dependent: :destroy, inverse_of: :mentioned
+  has_many :mentioneds, class_name: 'ReportMention', foreign_key: 'mentioning_id', dependent: :destroy, inverse_of: :mentioning
 
-  has_many :mentioning_reports, through: :mentioning, source: :mentioning
-  has_many :mentioned_reports, through: :mentioned, source: :mentioned
+  has_many :mentioning_reports, through: :mentionings, source: :mentioning
+  has_many :mentioned_reports, through: :mentioneds, source: :mentioned
 
   validates :title, presence: true
   validates :content, presence: true
@@ -28,7 +28,7 @@ class Report < ApplicationRecord
 
   def mention
     keys = content.scan(%r{(?:http://localhost:3000/reports/)(\d+)}).flatten
-    keys.each { |key| mentioning.create(mentioning_id: key) } unless keys.empty?
+    keys.each { |key| mentionings.create(mentioning_id: key) } unless keys.empty?
   end
 
   def update_mention
@@ -36,7 +36,7 @@ class Report < ApplicationRecord
     after_keys = content.scan(%r{(?:http://localhost:3000/reports/)(\d+)}).flatten
     deleted_keys = before_keys.difference(after_keys)
     new_keys = after_keys.difference(before_keys)
-    new_keys.each { |key| mentioning.create(mentioning_id: key) } unless new_keys.empty?
-    deleted_keys.each { |key| mentioning.find_by(mentioning_id: key).destroy } unless deleted_keys.empty?
+    new_keys.each { |key| mentionings.create(mentioning_id: key) } unless new_keys.empty?
+    deleted_keys.each { |key| mentionings.find_by(mentioning_id: key).destroy } unless deleted_keys.empty?
   end
 end
