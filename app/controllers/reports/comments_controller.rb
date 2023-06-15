79a1cfacf657197dda1098a5ef89b588fce808f8
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-15 10:06:59 +0900

    reportのインスタンス変数を使い忘れていたので修正

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -3,19 +3,18 @@
 class Reports::CommentsController < ApplicationController
   before_action :set_report, only: %i[create destroy]
   def create
-    report = Report.find(params[:report_id])
-    if report.comments.build(content: comment_params[:content], user: current_user).save
-      redirect_to report_url(report), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    if @report.comments.build(content: comment_params[:content], user: current_user).save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      render book_url(reoprt), status: :bad_request
+      render report_url(@reoprt), status: :bad_request
     end
   end
 
   def destroy
     if current_user.comments.find(params[:id]).destroy
-      redirect_to report_url(params[:id]), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
     else
-      render report_url(params[:id]), status: :bad_request
+      render report_url(@report), status: :bad_request
     end
   end
 
