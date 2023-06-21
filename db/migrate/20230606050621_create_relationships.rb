226c7966a75fd52cdb16023f506ff1cacb7910b0
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-21 13:30:13 +0900

    uniqをuniqueに修正

--- db/migrate/20230606050621_create_relationships.rb
+++ db/migrate/20230606050621_create_relationships.rb
@@ -3,7 +3,7 @@ class CreateRelationships < ActiveRecord::Migration[7.0]
     create_table :report_mentions do |t|
       t.references :mentioning, null: false, foreign_key: { to_table: :reports}
       t.references :mentioned, null: false, foreign_key: { to_table: :reports}
-      t.index ["mentioning_id", "mentioned_id"], name: "index_mentioning_on_mentioned", uniq: true
+      t.index ["mentioning_id", "mentioned_id"], name: "index_mentioning_on_mentioned", unique: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_081151) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_050621) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_081151) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_081151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mentioned_id"], name: "index_report_mentions_on_mentioned_id"
-    t.index ["mentioning_id", "mentioned_id"], name: "index_report_mentions_on_mentioning_id_and_mentioned_id", unique: true
+    t.index ["mentioning_id", "mentioned_id"], name: "index_mentioning_on_mentioned", unique: true
     t.index ["mentioning_id"], name: "index_report_mentions_on_mentioning_id"
   end
 
